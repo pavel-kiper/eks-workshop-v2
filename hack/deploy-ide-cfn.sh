de5d9fbda187f3a82e75f44c925782e1ee3be320
Niall Thomson <thomson.niall@gmail.com>
2026-04-23 21:27:12 +0000

    Hide IDE right sidebar by default

--- hack/deploy-ide-cfn.sh
+++ hack/deploy-ide-cfn.sh
@@ -12,6 +12,20 @@ bash $SCRIPT_DIR/build-ide-cfn.sh $outfile
 
 source $SCRIPT_DIR/lib/resolve-source-ip.sh
 
-aws cloudformation deploy --stack-name "$EKS_CLUSTER_NAME-ide" \
+STACK_NAME="$EKS_CLUSTER_NAME-cfn"
+
+aws cloudformation deploy --stack-name "$STACK_NAME" \
   --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-file $outfile \
   --parameter-overrides InboundCIDR="$INBOUND_CIDRS"
+
+if [ -z "$CI" ]; then
+  IDE_URL=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" \
+    --query 'Stacks[0].Outputs[?OutputKey==`IdeUrl`].OutputValue' --output text)
+
+  IDE_PASSWORD=$(aws secretsmanager get-secret-value \
+    --secret-id "$STACK_NAME-password" --query 'SecretString' --output text | jq -r '.password')
+
+  echo ""
+  echo "IDE URL:      $IDE_URL"
+  echo "IDE Password: $IDE_PASSWORD"
+fi
